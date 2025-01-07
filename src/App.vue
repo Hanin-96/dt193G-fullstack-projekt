429ecf8e0df924dcd46d08ba0f09b90d77f38db4
Stina Persson <stpe1901@student.miun.se>
2025-01-07 12:04:25 +0100

    Kopplat formulär med v-model för registrering i komponent registeruser. Buggfixar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <Header />
   <main> 
-    <Routerview />
+    <RouterView />
   </main>
   <Footer />
 </template>
--- src/components/RegisterUser.vue
+++ src/components/RegisterUser.vue
@@ -3,15 +3,57 @@
 <template>
     <h1>Apotek 24/7</h1>
     <form>
-        <label for="firstname">Förnamn test:</label><br>
-        <input type="text" name="firstname" id="firstname">
+        <label for="firstname">Förnamn:</label><br>
+        <input v-model="firstname" type="text" name="firstname" id="firstname"><br>
+
+        <label for="lastname">Efternamn:</label><br>
+        <input v-model="lastname" type="text" name="lastnamn" id="lastname"><br> 
+
+        <label for="email">Epost:</label><br>
+        <input v-model="email" type="text" name="email" id="email"><br> 
+
+        <label for="username">Användarnamn:</label><br>
+        <input v-model="username" type="text" name="username" id="username"><br> 
+
+        <label for="password">Lösenord:</label><br>
+        <input v-model="password" type="text" name="password" id="password"><br>
+        <br>
+        <input type="submit" value="Registrera"><br>
+
     </form>
 </template>
 
 <script>
-
+export default {
+    data() {
+        return {
+            firstname: "koppling", 
+            lastname: "test", 
+            email: "att", 
+            username: "det", 
+            password: "funkar"
+        }
+    }
+}
 </script>
 
 <style scoped>
+h1 {
+    background-color: #ffffff;
+    color: #353535; 
+    padding: 1em; 
+    text-align: center;
+} 
 
+input {
+    width: 355px; 
+    max-width: 100%;
+    margin: 0 auto; 
+    padding: 1em; 
+    border-radius: 5px;
+} 
+
+form {
+    color: #353535; 
+}
 </style>
