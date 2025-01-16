a1fe14bd070bbe3636943adbdd84fc6daff62113
Hanin Farhan <haninfarhan96@gmail.com>
2025-01-16 14:56:17 +0100

    Test 1- fixar buggar med problem i publicering, tog bort home view

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import LoginView from '@/views/LoginView.vue'
 import AdminView from '@/views/AdminView.vue'
@@ -8,21 +7,14 @@ import LogoutView from '@/views/LogoutView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-      redirect: {
-        name: 'login'
-      }
-    },
+   
     {
       path: '/register',
       name: 'register',
       component: RegisterView
     },
     {
-      path: '/login',
+      path: '/',
       name: 'login',
       component: LoginView
     },
